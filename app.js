4833c49e3844fdded58788ee0ec1c3d78ca3a824
DX <dxcode01@gmail.com>
2024-06-03 02:10:15 -0500

    css rework using BEM , still sucks but sucks little less js sucks , need rework using pub & sub pattern and module pattern

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = {
 
         //component 
         let component =
-            `<div class="book" id = "${title}">
+            `<div class="collection__card card--layout" id = "${title}">
                 <p>${title}</p>
             </div>`;
 
@@ -45,9 +45,39 @@ const app = {
     setNewBookForm: function () {
         //set form
         let component =
-            `<form action="#" method = "get" id="new-book-form" >
-            <div>
-                <label for="description">Description*</label>
+            `<form action="#" method = "get" id="new-book-form">
+            <div class="form__section">
+                    <label for="title" class="form__section__label label--greenTheme">Title*</label>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="title" 
+                        placeholder="Title*"
+                        required
+                    >
+            </div>
+            <div class="form__section">
+                    <label for="title" class="form__section__label label--greenTheme">Year*</label>
+                    <input 
+                        type="text" 
+                        name="year" 
+                        id="year"
+                        placeholder="Year*" 
+                        required
+                    >
+            </div>
+            <div class="form__section">
+                    <label for="author" class="form__section__label label--greenTheme">Author*</label>
+                    <input 
+                        type="text" 
+                        name="autor" 
+                        id="author"
+                        placeholder="Author*" 
+                        required
+                    >
+            </div>
+            <div class="form__section">
+                <label for="description" class="form__section__label label--greenTheme">Description*</label>
                 <textarea 
                     type="text" 
                     name="description" 
@@ -57,41 +87,9 @@ const app = {
                 >
                 </textarea>
             </div>
-            <div>
-                <div class="form-field">
-                    <label for="title">Title*</label>
-                    <input 
-                        type="text" 
-                        name="title" 
-                        id="title" 
-                        placeholder="Title*"
-                        required
-                    >
-                </div>
-                <div class="form-field">
-                    <label for="title">Year*</label>
-                    <input 
-                        type="text" 
-                        name="year" 
-                        id="year"
-                        placeholder="Year*" 
-                        required
-                    >
-                </div>
-                <div class="form-field">
-                    <label for="author">Author*</label>
-                    <input 
-                        type="text" 
-                        name="autor" 
-                        id="author"
-                        placeholder="Author*" 
-                        required
-                    >
-                </div>
-                <div class="form-field">
-                    <button>Add to Collection</button>
-                </div>
-            </div>
+            <div class="form__section">
+                <button class="bttn bttn--mediumSize bttn--greenTheme">Add to Collection</button>
+            </div>    
         </form>`;
 
         //append form to content
@@ -104,14 +102,15 @@ const app = {
     setDelBookForm: function () {
         //component
         let component = 
-        `<div id="del-book-form">
-        <div class = 'del-form-field'>
-            <label for="titel">Title*</label>
+        `<form id="del-book-form">
+        <div class = 'form__section'>
+            <label for="titel" class="form__section__label label--redTheme"> Title*</label>
             <input type="text" name="title" id="title">
         </div>
-        <div class = "del-form-field">
-            <button>Delete Book</button>
-        </div>`;
+        <div class = "form__section">
+            <button class = "bttn bttn--mediumSize bttn--redTheme">Delete Book</button>
+        </div> 
+        </form>`;
         //add to doom
         let content = document.querySelector('#content');
         content.innerText = '';
@@ -163,10 +162,16 @@ const app = {
     showBookInfo: function (id) {
         //set component
         let component = `
-            <div id = 'bookInfo'>
-                <h1>${this.books[id].title}</h1>
+        <div id = 'bookInfo'>
+            <div class="bookInfo__section bookInfo__section--headers">
+                <p>Title:${this.books[id].title}</p>
+                <p>Author:${this.books[id].author}</p>
+                <p>Year:${this.books[id].year}</p>
+            </div>
+            <div class = 'bookInfo__section bookInfo__section--content'>
                 <p>${this.books[id].content}</p>
             </div>
+        </div>
         `;
         //clean #content 
         document.querySelector("#content").innerText = '';
@@ -205,15 +210,10 @@ const app = {
             }
             //empty content
             document.querySelector('#content').innerText = '';
+            
         });
     },
     setEventListeners: function () {
-        // set sample books
-        [{title : "Robin Hood" , desc : "bla bla bla"} , {title : "The Hobit" , desc : "bla bla bla"}].forEach(book =>{
-            for(let i = 0 ; i<= 4 ; i++){
-                app.setBook(book.title , book.desc);
-            }
-        })
 
         let nav = document.querySelector('#nav');
         let aside = document.querySelector('#aside');
@@ -239,11 +239,9 @@ const app = {
 
         //main-> aside events
         aside.addEventListener('click', eve => {
-            switch (eve.target.getAttribute('class')) {
-                //show book info 
-                case 'book':
-                    this.showBookInfo(eve.target.getAttribute('id'));
-                    break;
+            if(eve.target.classList.contains("collection__card")) {
+                //show book info
+                this.showBookInfo(eve.target.getAttribute('id'));
             }
         });
 
@@ -254,6 +252,15 @@ const app = {
     },
 
 }
-
-
 app.setEventListeners();
+
+//example book
+let content = 
+`Robin Hood is a legendary heroic outlaw originally depicted in English folklore and subsequently featured in literature, theatre, and cinema. According to legend, he was a highly skilled archer and swordsman.[1] In some versions of the legend, he is depicted as being of noble birth, and in modern retellings he is sometimes depicted as having fought in the Crusades before returning to England to find his lands taken by the Sheriff. In the oldest known versions, he is instead a member of the yeoman class. Traditionally depicted dressed in Lincoln green, he is most famous for his attribute of stealing from the rich to give to the poor.
+Through retellings, additions, and variations, a body of familiar characters associated with Robin Hood has been created. These include his lover, Maid Marian; his band of outlaws, the Merry Men; and his chief opponent, the Sheriff of Nottingham. The Sheriff is often depicted as assisting Prince John in usurping the rightful but absent King Richard, to whom Robin Hood remains loyal. He became a popular folk figure in the Late Middle Ages, and his partisanship of the common people and opposition to the Sheriff are some of the earliest-recorded features of the legend, whereas his political interests and setting during the Angevin era developed in later centuries. The earliest known ballads featuring him are from the 15th century.
+There have been numerous variations and adaptations of the story over the subsequent years, and the story continues to be widely represented in literature, film, and television media today. Robin Hood is considered one of the best-known tales of English folklore. In popular culture, the term "Robin Hood" is often used to describe a heroic outlaw or rebel against tyranny.
+The origins of the legend as well as the historical context have been debated for centuries. There are numerous references to historical figures with similar names that have been proposed as possible evidence of his existence, some dating back to the late 13th century. At least eight plausible origins to the story have been mooted by historians and folklorists, including suggestions that "Robin Hood" was a stock alias used by or in reference to bandits.`;
+let title = 'Robin Hood';
+let author = 'Anonymous balladeers';
+let year = '1300';
+app.setBook(title, author , year , content);
